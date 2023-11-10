078b38f2c8bdcedeea373a737e3645423eec3246
Rmoney3663 <75226404+Rmoney3663@users.noreply.github.com>
2023-11-10 12:49:27 -0500

    test

---
+++ ProjetWebFinale/Models/DVDDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetWebFinale.Models
+{
+    public class DVDDbContext
+    {
+        public DVDDbContext(DbContextOptions<DVDDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+        public DbSet<DVD> DVD { get; set; }
+        public DbSet<Categorie> Categorie { get; set; }
+
+    }
+}
