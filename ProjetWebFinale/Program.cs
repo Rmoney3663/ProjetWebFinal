02bfd3fa2ea3bd9243857450269f982556934dab
Jerome <47067008+RNGPuns@users.noreply.github.com>
2023-11-13 19:07:42 -0500

    Revert "Fix identity bug before commit and merge"
    This reverts commit 71ff5998d50ee8dd84c2c63cc5e51a2a48cdb875.

--- ProjetWebFinale/Program.cs
+++ ProjetWebFinale/Program.cs
@@ -1,17 +1,8 @@
 using ProjetWebFinale.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using ProjetWebFinale.Data;
-using ProjetWebFinale.Areas.Identity.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Connection");
-
 builder.Services.AddDbContext<FilmDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
-
-builder.Services.AddDefaultIdentity<ProjetWebFinaleUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ProjetWebFinaleContext>();
 builder.Services.AddControllersWithViews();
 
 // Add services to the container.
@@ -32,7 +23,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
 
 app.UseAuthorization();
 
