c275038cbc9d42388d21edab272c200a8522a4fd
Jerome <jerlab8@gmail.com>
2023-12-04 14:10:44 -0500

    Revert "Revert "merge""
    This reverts commit fa86180f78525f433c2cd0c2de6e37774c5cb7a6.

--- ProjetWebFinale/Migrations/20231130190020_fixkey.Designer.cs
+++ ProjetWebFinale/Migrations/20231130190020_fixkey.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetWebFinale.Models;
 
@@ -11,9 +12,10 @@ using ProjetWebFinale.Models;
 namespace ProjetWebFinale.Migrations
 {
     [DbContext(typeof(FilmDbContext))]
-    partial class FilmDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231130190020_fixkey")]
+    partial class fixkey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjetWebFinale/Migrations/20231130190020_fixkey.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetWebFinale.Migrations
+{
+    public partial class fixkey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Films_Categories_NoUtilisateurProprietaire",
+                table: "Films");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Xtra",
+                table: "Films",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Films_Categorie",
+                table: "Films",
+                column: "Categorie");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Films_Categories_Categorie",
+                table: "Films",
+                column: "Categorie",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Films_Categories_Categorie",
+                table: "Films");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Films_Categorie",
+                table: "Films");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Xtra",
+                table: "Films",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Films_Categories_NoUtilisateurProprietaire",
+                table: "Films",
+                column: "NoUtilisateurProprietaire",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
