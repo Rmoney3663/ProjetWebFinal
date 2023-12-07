e285a63fff9dbca8cbb2084091ee3fdd1d3a5d89
Jerome <jerlab8@gmail.com>
2023-12-07 14:12:51 -0500

    Changes
    Moved contact to another page and added message if query returns no results

---
+++ ProjetWebFinale/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjetWebFinale.Models;
+
+namespace ProjetWebFinale.Controllers
+{
+    [Authorize]
+    public class EmailController : Controller
+    {
+        private readonly FilmDbContext _context;
+
+        public EmailController(FilmDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Email()
+        {
+            //var utilisateurs = _context.Utilisateurs.ToList();
+            var utilisateur = from u in _context.Utilisateurs where u.TypeUtilisateur == 3 select u;
+
+            ViewData["Courriel"] = new SelectList(utilisateur, "Courriel", "Courriel");
+
+            return View();
+        }
+
+
+
+
+    }
+}
