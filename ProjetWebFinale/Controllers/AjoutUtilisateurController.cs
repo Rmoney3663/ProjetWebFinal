67ea6f8e12b05fbf73a590ebc4a28f2f000a3e95
Chipsy0 <99271702+Chipsy0@users.noreply.github.com>
2023-12-18 16:29:50 -0500

    Add user par admin FINI

--- ProjetWebFinale/Controllers/AjoutUtilisateurController.cs
+++ ProjetWebFinale/Controllers/AjoutUtilisateurController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,11 +12,15 @@ namespace ProjetWebFinale.Controllers
 {
     public class AjoutUtilisateurController : Controller
     {
+        private readonly SignInManager<Utilisateurs> signInManager;
         private readonly FilmDbContext _context;
+        private readonly UserManager<Utilisateurs> userManager;
 
-        public AjoutUtilisateurController(FilmDbContext context)
+        public AjoutUtilisateurController(FilmDbContext context, UserManager<Utilisateurs> userManager, SignInManager<Utilisateurs> signInManager)
         {
             _context = context;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         // GET: AjoutUtilisateur
@@ -47,7 +52,8 @@ namespace ProjetWebFinale.Controllers
         // GET: AjoutUtilisateur/Create
         public IActionResult Create()
         {
-            ViewData["TypeUtilisateur"] = new SelectList(_context.TypesUtilisateur, "Identifiant", "Description");
+            IEnumerable<TypesUtilisateur> liste = _context.TypesUtilisateur.Except(_context.TypesUtilisateur.Where(u => u.Identifiant == 'A'));
+            ViewData["TypeUtilisateur"] = new SelectList(liste, "Identifiant", "Description");
             return View();
         }
 
@@ -56,16 +62,77 @@ namespace ProjetWebFinale.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,NomUtilisateur,Courriel,MotPasse,TypeUtilisateur,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Utilisateurs utilisateurs)
+        public async Task<IActionResult> Create([Bind("Nom,Courriel,MotDePasse,ConfirmerMotDePasse,TypeUtilisateur")] RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(utilisateurs);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                // Copy data from RegisterViewModel to IdentityUser
+                var noType = 100;
+                if (model.TypeUtilisateur == 'S')
+                    noType = 2;
+                else if (model.TypeUtilisateur == 'U')
+                    noType = 3;
+                var user = new Utilisateurs
+                {
+                    UserName = model.Nom,
+                    Email = model.Courriel,
+                    Courriel = model.Courriel,
+                    NomUtilisateur = model.Nom,
+                    TypeUtilisateur = noType,
+                    EmailConfirmed = true
+                };
+                // Store user data in AspNetUsers database table
+                var result = await userManager.CreateAsync(user, model.MotDePasse);
+                // If user is successfully created, sign-in the user using. SignInManager and redirect to index action of HomeController
+                if (result.Succeeded)
+                {
+                    //Sets default preferences on first login
+                    var pref1 = new UtilisateursPreferences
+                    {
+                        NoUtilisateur = user.Id,
+                        NoPreference = 3,
+                        Valeur = "oui"
+                    };
+
+                    var pref2 = new UtilisateursPreferences
+                    {
+                        NoUtilisateur = user.Id,
+                        NoPreference = 4,
+                        Valeur = "oui"
+                    };
+
+                    var pref3 = new UtilisateursPreferences
+                    {
+                        NoUtilisateur = user.Id,
+                        NoPreference = 5,
+                        Valeur = "oui"
+                    };
+
+                    var pref4 = new UtilisateursPreferences
+                    {
+                        NoUtilisateur = user.Id,
+                        NoPreference = 7,
+                        Valeur = "12"
+                    };
+                    _context.UtilisateursPreferences.Add(pref1);
+                    _context.UtilisateursPreferences.Add(pref2);
+                    _context.UtilisateursPreferences.Add(pref3);
+                    _context.UtilisateursPreferences.Add(pref4);
+                    await _context.SaveChangesAsync();
+
+                    await signInManager.SignOutAsync();
+                    
+                    return RedirectToAction("Login", "Account");
+                }
+                // If there are any errors, add them to the ModelState object. which will be displayed by the validation summary tag helper
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
-            ViewData["TypeUtilisateur"] = new SelectList(_context.TypesUtilisateur, "Identifiant", "Description", utilisateurs.TypeUtilisateur);
-            return View(utilisateurs);
+            IEnumerable<TypesUtilisateur> liste = _context.TypesUtilisateur.Except(_context.TypesUtilisateur.Where(u => u.Identifiant == 'A'));
+            ViewData["TypeUtilisateur"] = new SelectList(liste, "Identifiant", "Description");
+            return View(model);
         }
 
         // GET: AjoutUtilisateur/Edit/5
